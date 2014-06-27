f987144f74c5d2f71e145731895b4397cd68f58c
Allyriane Launois <allyriane.launois@epitech.eu>
2014-06-27 17:00:53 +0200

    lib list : attribute nonnull

--- libs/list/inc/list.h
+++ libs/list/inc/list.h
@@ -19,25 +19,33 @@ typedef struct	s_list
 
 }		t_list;
 
-t_node	*create_node(void *data);
-void	destroy_node(t_node *node, bool free_data);
+t_node	*create_node(void *data) __attribute__ ((nonnull (1)));
+void	destroy_node(t_node *node, bool free_data)
+  __attribute__ ((nonnull (1)));
 
 t_list	*list_create(void);
-void	list_destroy(t_list *list, bool free_data);
+void	list_destroy(t_list *list, bool free_data)
+  __attribute__ ((nonnull (1)));
 
-int	list_size(t_list *list);
+int	list_size(t_list *list) __attribute__ ((nonnull (1)));
 
-void	*list_front(t_list *list);
+void	*list_front(t_list *list) __attribute__ ((nonnull (1)));
 
-bool	list_push_front(t_list *list, void *data);
-void	list_pop_front(t_list *list, bool free_data);
+bool	list_push_front(t_list *list, void *data)
+  __attribute__ ((nonnull (1, 2)));
+void	list_pop_front(t_list *list, bool free_data)
+  __attribute__ ((nonnull (1)));
 
-bool	list_push_back(t_list *list, void *data);
+bool	list_push_back(t_list *list, void *data)
+  __attribute__ ((nonnull (1, 2)));
 
-bool	list_insert(t_list *list, int pos, void *data);
-void	list_remove(t_list *list, void *data, bool free_data);
+bool	list_insert(t_list *list, int pos, void *data)
+  __attribute__ ((nonnull (1, 3)));
+void	list_remove(t_list *list, void *data, bool free_data)
+  __attribute__ ((nonnull (1, 2)));
 
-void	list_for_each(t_list *list, void (*fcn)(void *));
+void	list_for_each(t_list *list, void (*fcn)(void *))
+  __attribute__ ((nonnull (1)));
 void	list_for_each_arg(t_list *list, void (*fcn)(void *, void *), void *param);
 
 #endif		/* !LIST_H_ */
