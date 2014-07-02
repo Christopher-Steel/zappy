0214cacb1cdd37acda12dbb5d4e0f07b5d9c788d
Allyriane Launois <allyriane.launois@epitech.eu>
2014-07-02 18:40:20 +0200

    list_clear.c

---
+++ libs/list/src/list_clear.c
@@ -0,0 +1,16 @@
+
+#include "list.h"
+
+void		list_clear(t_list *list, bool free_data)
+{
+  t_node	*tmp;
+  t_node	*node;
+
+  node = list->nodes;
+  while (node)
+    {
+      tmp = node->next;
+      destroy_node(node, free_data);
+      node = tmp;
+    }
+}
