0d54ddb59991c5ec578e0ed23bbf457be7f94708
Allyriane Launois <allyriane.launois@epitech.eu>
2014-07-09 13:38:37 +0200

    graphic : msz, bct

--- server/modules/Graphic/make_module.mk
+++ server/modules/Graphic/make_module.mk
@@ -2,7 +2,10 @@ GRAPHIC_DIR	:=	$(dir $(lastword $(MAKEFILE_LIST)))
 
 ##########################################################################
 
-GRAPHIC		=	$(SRC_FOLDER)graphic_create.c
+GRAPHIC		=	$(SRC_FOLDER)graphic_create.c	\
+			$(SRC_FOLDER)graphic_msz.c	\
+			$(SRC_FOLDER)graphic_bct.c	\
+			#$(SRC_FOLDER)graphic_ .c
 
 ##########################################################################
 
---
+++ server/modules/Graphic/src/graphic_bct.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+
+#include "graphic.h"
+#include "print_error.h"
+#include "server.h"
+
+static bool	get_position(char *cmd, uint *x, uint *y)
+{
+  char		*tmp;
+
+  tmp = strtok(cmd, " \n");
+  if (tmp)
+    *x = atoi(tmp);
+  else
+    return (false);
+
+  tmp = strtok(NULL, " \n");
+  if (tmp)
+    *y = atoi(tmp);
+  else
+    return (false);
+  return (true);
+}
+
+bool	graphic_bct(t_graphic *graphic, char *cmd)
+{
+  uint	x;
+  uint	y;
+  bool	success;
+  char	*answer;
+  uint	*res;
+
+  if (!get_position(cmd, &x, &y))
+    return (printf_error("\"%s\" is a bad case position", strtok(cmd, "\n")));
+  if (x < gs_get_map_width() && gs_get_map_height())
+    return (print_error("invalid map position"));
+  res = g_server.world->cell[(x + (y * gs_get_map_width()))].res;
+  asprintf(&answer, "bct %u %u %u %u %u %u %u %u %u\n", x, y, res[0], res[1],
+	   res[2], res[3], res[4], res[5], res[6]);
+  success = client_write_to(graphic->client, answer);
+  free(answer);
+  return (success);
+}
