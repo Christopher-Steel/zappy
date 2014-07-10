5b4a2dcb44e964f9bdb417c6b359fe712b8abd18
Allyriane Launois <allyriane.launois@epitech.eu>
2014-07-10 11:50:16 +0200

    team: add team_remove_player/egg

--- server/modules/Team/make_module.mk
+++ server/modules/Team/make_module.mk
@@ -8,7 +8,8 @@ TEAM		=	$(SRC_FOLDER)team_list_init.c		\
 			$(SRC_FOLDER)team.c			\
 			$(SRC_FOLDER)team_add_player.c		\
 			$(SRC_FOLDER)team_create_player.c	\
-			$(SRC_FOLDER)team_update_max_level.c
+			$(SRC_FOLDER)team_update_max_level.c	\
+			$(SRC_FOLDER)team_remove_player.c
 
 ############################################################
 
